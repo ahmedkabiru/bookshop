83402e03aae570e72cd9d9edbda34a6fb09dad2f
Ahmed  Kabiru <aopeyemikabiru@gmail.com>
2024-09-23 22:37:48 +0200

    update

--- catalog-service/build.gradle.kts
+++ catalog-service/build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.retry:spring-retry")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
