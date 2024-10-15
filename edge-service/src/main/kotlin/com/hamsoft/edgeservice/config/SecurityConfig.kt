cdcdf65395908bdb1adf814717c06bc774015464
Ahmed  Kabiru <aopeyemikabiru@gmail.com>
2024-10-15 20:08:39 +0200

    create SecurityConfig

---
+++ edge-service/src/main/kotlin/com/hamsoft/edgeservice/config/SecurityConfig.kt
@@ -0,0 +1,20 @@
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@EnableWebFluxSecurity
+@Configuration
+class SecurityConfig{
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return  http.authorizeExchange {
+            exchange -> exchange.anyExchange().authenticated()
+        }.oauth2Login(Customizer.withDefaults()).build()
+    }
+
+}
